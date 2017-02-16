bd303a5764f3375c64bc5776a57fb520ce775a1c
SDM Member <pravesh.technolabs@gmail.com>
2017-02-16 13:09:03 +0530

    initial commit

---
+++ src/main/java/springmongodbdatarest/ProductRepository.java
@@ -0,0 +1,9 @@
+package springmongodbdatarest;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
